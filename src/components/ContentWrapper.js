b1fd3ea7d359f96a07031e25b901e95b876f9acd
GabrielAmestoy <gabrielamestoy1993@gmail.com>
2021-11-24 22:10:37 -0200

    creación de aplicación con React

---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop'
+import Footer from './Footer'
+
+
+const ContentWrapper = () => {
+    return (
+        <div>
+            <TopBar />
+            <ContentRowTop />
+            <Footer />
+        </div>
+    );
+}
+
+export default ContentWrapper;
